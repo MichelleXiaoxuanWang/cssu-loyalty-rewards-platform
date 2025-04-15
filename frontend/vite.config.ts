f7483fcad2b978c2ebaafb451ca7080cd18ed984
Michelle Wang <xxw0922@gmail.com>
2025-04-14 21:58:34 -0400

    alow railway domain for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	preview: {
+		port: 4173,
+		host: true,
+		allowedHosts: [
+			"309projfrontend-production.up.railway.app",
+			"localhost"
+		]
+	},
+	server: {
+		port: 4173,
+		host: true
+	}
 });
